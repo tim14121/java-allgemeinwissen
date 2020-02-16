01f6cec0fe11639c04e03b766b8a7ef627263854
Tim S <timeyy@hotmail.de>
2020-02-16 20:30:52 +0100

    Initial commit

---
+++ src/main/java/com/java/allgemein/wissen/JavaAllgemeinwissenApplication.java
@@ -0,0 +1,13 @@
+package com.java.allgemein.wissen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAllgemeinwissenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAllgemeinwissenApplication.class, args);
+	}
+
+}
