ef92fd093c78f1d767dcdeaa4ad05e8c757a1c3d
Tim Schmey <t.schmey@cartelsol.com>
2020-03-30 11:39:57 +0200

    ConsumerClass

--- src/main/java/com/java/allgemein/wissen/functional/programming/ConsumerClass.java
+++ src/main/java/com/java/allgemein/wissen/functional/programming/ConsumerClass.java
@@ -11,26 +11,31 @@ public class ConsumerClass {
         consumerClass.createBiConsumerFunction();
     }
 
-    public void createConsumerFunction(){
-        Consumer<User> greetingFunction= user -> System.out.println("Hello "
-                + user.name + " from " + user.city);
+    public void createConsumerFunction() {
+        Consumer<User> greetingFunction = user -> System.out.println("Hello " + user.name + " from " + user.city);
 
         greetingFunction.accept(new User("Tim", "Berlin"));
     }
 
-    public void createBiConsumerFunction(){
-        BiConsumer<User,Boolean> greetingFctWithBoolean = (user, shouldShowCity) ->
-                System.out.println("Hello " + user.name + " from " + (shouldShowCity? user.city: "****"));
+    public void createBiConsumerFunction() {
+        BiConsumer<User, Boolean> greetingFctWithBoolean = (user, shouldShowCity) -> System.out.println("Hello " + user.name + " from " + (shouldShowCity ? user.city : "****"));
+        greetingFctWithBoolean.accept(new User("Tim", "Berlin"), false);
+        greetingFctWithBoolean.accept(new User("Jino", "Berlin"), true);
 
-        greetingFctWithBoolean.accept(new User("Tim", "Berlin"),false);
-        greetingFctWithBoolean.accept(new User("Jino", "Berlin"),true);
+        greetingWithCity((user, shouldShowCity) -> System.out.println("Hello " + user.name + " from " + (shouldShowCity ? user.city : "****")), new User("Üsame", "Berlin"),
+
+            false);
     }
 
+    public void greetingWithCity(BiConsumer<User, Boolean> biConsumer, User user, boolean shouldShowCity) {
+        biConsumer.accept(user, shouldShowCity);
+    }
     static class User {
 
 
         final String name;
         final String city;
+
         public User(String name, String city) {
             this.name = name;
             this.city = city;
--- src/main/java/com/java/allgemein/wissen/functional/programming/escercises/Test.java
+++
@@ -1,7 +0,0 @@
-package com.java.allgemein.wissen.functional.programming.escercises;
-
-public class Test {
-    int a = 4;
-    String b;
-    double d = 5.0;
-}
